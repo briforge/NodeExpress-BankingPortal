3791576a7d84e3b7672b175f69eb84ab484b3e84
Brian Preston <Brian.Preston@salliemae.com>
2020-08-04 17:59:03 -0400

    fixed public static test

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public/')))
 
-app.get('/', (req, res) {
-  res.render('index', { title: 'Index'})
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Index'});
 })
 
-app.listen(3000);
-console.log('PS Project Running on port 3000!');
+app.listen(3000, function() {
+  console.log('PS Project Running on port 3000!');
+});
