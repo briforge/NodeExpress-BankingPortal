beae871c2f87b0ddfe7151e42ff9289170966cb8
Brian Preston <Brian.Preston@salliemae.com>
2020-08-04 16:52:37 -0400

    first try

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) {
+  res.render('index', { title: 'Index'})
+})
+
+app.listen(3000);
+console.log('PS Project Running on port 3000!');
