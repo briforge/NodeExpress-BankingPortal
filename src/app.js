8a976eea47673944c2d9f6e1d7096ed26ced65b0
Brian Preston <Brian.Preston@salliemae.com>
2020-08-06 16:56:51 -0400

    done module 3, transfer and payment

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public/')))
+app.use(express.urlencoded({ extended: true }))
 
 const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'),
                                             {encoding:'utf8'});
@@ -34,6 +35,51 @@ app.get('/credit', function(req, res) {
 
 app.get('/profile', function(req, res) {
   res.render('profile', { user: users[0]} );
+})
+
+app.get('/transfer', function(req, res) {
+  res.render('transfer');
+})
+
+app.post('/transfer', function(req, res) {
+  console.log(req.body);
+  var xferAmt = parseInt(req.body.amount);
+  var fromAccount = accounts[req.body.from];
+  console.log(fromAccount);
+  var oldBalance = fromAccount.balance;
+  var newBalance = oldBalance - xferAmt;
+  console.log('new balance From Acct: ' + newBalance);
+  fromAccount.balance = newBalance;
+  console.log('new balance in accounts, From account : ' + accounts[req.body.from].balance);
+
+  var toAccount = accounts[req.body.to];
+  console.log('To Account : ' + toAccount);
+  var oldToBalance = toAccount.balance;
+  var newToBalance = oldToBalance + xferAmt;
+  console.log('new balance To Acct: ' + newToBalance);
+  toAccount.balance = newToBalance;
+  console.log('new balance in accounts, To account : ' + accounts[req.body.to].balance);
+
+  var accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON,
+                              'utf8' );
+
+  res.render('transfer', { message: 'Transfer Completed'});
+})
+
+app.get('/payment', function(req, res) {
+  res.render('payment', { account: accounts.credit } );
+})
+
+app.post('/payment', function(req, res) {
+  var amt = parseInt(req.body.amount);
+  accounts.credit.balance -= amt;
+  accounts.credit.available += amt;
+  var accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON,
+                              'utf8' );
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit});
 })
 
 app.listen(3000, function() {
